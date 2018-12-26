1d587a5494c7c4a069c9f238252c6af149ee19e9
Lucas <lucas_s.s@hotmail.com>
2018-12-26 17:52:16 -0300

    First commit

---
+++ packages/shared/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: "airbnb",
+  parser: "babel-eslint",
+  env: { jest: true },
+  globals: { fetch: false },
+  rules: {
+    "react/jsx-wrap-multilines": 0,
+    "react/jsx-filename-extension": 0,
+    "no-use-before-define": 0,
+    "react/prop-types": 0
+  }
+};

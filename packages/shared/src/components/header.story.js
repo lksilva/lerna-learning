c28be988cea3d4d982e487b21324150534cf7d0d
Lucas <lucas_s.s@hotmail.com>
2018-12-29 12:58:00 -0300

    Corrigido links do storybooks e teste

--- packages/shared/src/components/header.story.js
+++ packages/shared/src/components/header.story.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { storiesOf } from '@storybook/react';
-import Header from './header';
+import { Header } from './header';
 
 storiesOf('Header', module).add('with title', () => <Header>React Template</Header>);

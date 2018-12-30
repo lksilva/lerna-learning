f4fa8ee4d1f06cfd8032e7d9efe49dde26fcb8c1
Lucas <lucas_s.s@hotmail.com>
2018-12-30 10:52:29 -0300

    Atualizado versões do react a removido sass do projeto

--- packages/elements/src/components/Button/FlatButton.js
+++ packages/elements/src/components/Button/FlatButton.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './button.css';
+import '../../styles/css/button.css';
 
 const FlatButton = ({ onClick, children }) => (
   <button className="btn" type="button" onClick={onClick}>
